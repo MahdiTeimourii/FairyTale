d0a2c6012af890385c5b2030822af3657c412594
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-07-11 12:01:48 -0700

    some cleaning

--- backend/routes/storyRoutes.js
+++ backend/routes/storyRoutes.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { generateStory } = require('./generateStory.js');
+const { generateStory } = require("../lib/generateStory.js");
 
-router.post('/generate-story', async (req, res) => {
+router.post("/generate-story", async (req, res) => {
   try {
     const story = await generateStory(req.body);
     res.json(story);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Error generating story' });
+    res.status(500).json({ error: "Error generating story" });
   }
 });
 
