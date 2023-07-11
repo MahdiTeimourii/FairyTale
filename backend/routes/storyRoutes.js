c27fbecfc629a776199cead2977e90c1477d8614
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-07-11 11:27:35 -0700

    Add story route

---
+++ backend/routes/storyRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { generateStory } = require('./generateStory.js');
+
+router.post('/generate-story', async (req, res) => {
+  try {
+    const story = await generateStory(req.body);
+    res.json(story);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error generating story' });
+  }
+});
+
+module.exports = router;
